bc40c2399f95d235fef0f5c3e903283b66b8755e
Spencer Tweedy <spencer@spencertweedy.com>
2020-04-06 10:31:13 -0500

    pass the title

--- _plugins/obs-archives.rb
+++ _plugins/obs-archives.rb
@@ -183,10 +183,17 @@ module Jekyll
 
     # Produce a title object suitable for Liquid based on type of archive.
     #
-    # Returns a String (for tag and category archives) and nil for
-    # date-based archives.
+    # Returns a String (for tag and category archives) and… something else for date archives
     def title
-      @title if @title.is_a? String
+      if @title.is_a? Hash
+        args = @title.values.map(&:to_i)
+        date = Date.new(*args)
+
+        @title = date.strftime("%B")
+        @title
+      elsif @title.is_a? String
+        @title
+      end
     end
 
     # Produce a date object if a date-based archive
