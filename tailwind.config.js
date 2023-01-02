ef0b64c276dab33325e2dda3fe894c40827270ab
Spencer Tweedy <spencertweedy@spencers-macbook-pro.local>
2023-01-02 16:27:01 +0000

    add page-in animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
     extend: {
       fontSize: {
         biggie: 'calc(1em + 2vw)'
+      },
+      animation: {
+        'enter': 'enter 0.4s'
+      },
+      keyframes: {
+        enter: {
+          '0%': { transform: 'scale(0.2)' },
+          '100%': { transform: 'scale(1)' }
+        }
       }
     }
   }
