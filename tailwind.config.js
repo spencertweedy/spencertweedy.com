4971517f57284c7228282a6baedbdfc89805aad2
Spencer Tweedy <spencertweedy@spencers-macbook-pro.local>
2023-02-04 23:35:04 +0000

    add trailing commas

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,22 +8,22 @@ module.exports = {
   theme: {
     fontFamily: {
       sans: ['jaf-facitweb', 'sans-serif'],
-      ft88: ['"FT88 Italic"']
+      ft88: ['"FT88 Italic"'],
     },
     extend: {
       colors: {
-        'obs-green': 'hsl(87, 47%, 52%)'
+        'obs-green': 'hsl(87, 47%, 52%)',
       },
       fontSize: {
-        biggie: 'calc(1.4em + 1.6vw)'
+        biggie: 'calc(1.4em + 1.6vw)',
       },
       animation: {
-        'enter': 'enter 0.4s'
+        'enter': 'enter 0.4s',
       },
       keyframes: {
         enter: {
           '0%': { transform: 'scale(0.2)' },
-          '100%': { transform: 'scale(1)' }
+          '100%': { transform: 'scale(1)' },
         }
       }
     }
